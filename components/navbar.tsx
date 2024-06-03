15f853a38aefbc5549fc78bf815b1e7033df9982
David <davidllumitaxi67@gmail.com>
2024-06-03 00:36:19 -0500

    first commit

---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="fixed z-20 flex-col items-center w-full mt-auto justify-center h-max bottom-20">
+            <div className="flex items-center gap-2 justify-center px-4 py-1 dark:bg-white/10 bg-slate-800/100 background-blur-sm rounded-full">
+
+            </div>
+
+        </nav>
+
+    );
+}
+
+export default Navbar;
